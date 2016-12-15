940dd55a9b9767beaafb33584d6fb91c049c3298
KV9801 <kva.923@gmail.com>
2016-12-15 18:04:58 +0100

    screw up bacon!

--- aosp_bacon.mk
+++ aosp_bacon.mk
@@ -40,6 +40,10 @@ TARGET_VENDOR_PRODUCT_NAME := bacon
 TARGET_VENDOR_DEVICE_NAME := A0001
 PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
 
+PRODUCT_PACKAGES += \
+ Launcher3 \
+ Camera2
+
 ## Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
